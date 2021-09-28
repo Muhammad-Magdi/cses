41ef1ea3be36499120b91952b6b99c0e739df4fa
muhammad-magdi <muhammad.magdi1024@gmail.com>
2021-09-29 00:05:53 +0200

    add 2413 recursive solution

---
+++ dynamic-programming/2413.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+const int N = 1e6 + 6, MOD = 1e9 + 7;
+
+#define onBit(msk, b) ((msk) | (1 << (b)))
+
+const int TOP_LEFT_WALL = 0b100;
+const int TOP_RIGHT_WALL = 0b001;
+const int TOP_WALL = TOP_LEFT_WALL | TOP_RIGHT_WALL;
+
+vector<int> validOptions[8] = {
+  { 0b000, 0b101 },
+  {},
+  { 0b010, 0b011, 0b110, 0b111},
+  { 0b010, 0b011, 0b110, 0b111},
+  {},
+  { 0b000, 0b010, 0b011, 0b101, 0b110, 0b111 },
+  { 0b010, 0b011, 0b110, 0b111 },
+  { 0b000, 0b010, 0b011, 0b101, 0b110, 0b111 },
+};
+
+int t, n;
+
+int mem[N][8];
+int solve(int i, int last) {
+  if (i == 0) return (last & TOP_WALL) == TOP_WALL;
+  int& ret = mem[i][last];
+  if (~ret) return ret;
+  ret = 0;
+  for (int validOption : validOptions[last]) {
+    ret = (ret + solve(i - 1, validOption)) % MOD;
+  }
+  return ret;
+}
+
+int main() {
+  scanf("%d", &t);
+  memset(mem, -1, sizeof mem);
+  while (t--) {
+    scanf("%d", &n);
+    printf("%d\n", solve(n, 0b101));
+  }
+  return 0;
+}
